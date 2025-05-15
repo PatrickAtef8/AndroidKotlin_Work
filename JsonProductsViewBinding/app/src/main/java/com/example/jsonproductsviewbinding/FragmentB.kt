791064bae3e0acbf3d72375e455c377aff6937db
Patrick Atef <patrickatefffa22@gmail.com>
2025-05-15 12:33:05 +0300

    Day6 ViewBinding

--- JsonProductsViewBinding/app/src/main/java/com/example/jsonproductsviewbinding/FragmentB.kt
+++ JsonProductsViewBinding/app/src/main/java/com/example/jsonproductsviewbinding/FragmentB.kt
@@ -8,7 +8,7 @@ import androidx.fragment.app.Fragment
 import com.example.jsonproductsviewbinding.databinding.FragmentBBinding
 
 class FragmentB : Fragment() {
-    lateinit var binding: FragmentBBinding
+    private lateinit var binding: FragmentBBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
